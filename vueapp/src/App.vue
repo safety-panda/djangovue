90c62fafa9260c5b3d845deeba2c1741f82ce56b
safety-panda <earmiffs+git@googlemail.com>
2022-11-14 13:13:27 +0000

    added some components and styling for dependency testing

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="I'm a component in Vue app"/>
 </template>
 
 <script>
@@ -22,5 +21,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: #FCC0C5;
 }
 </style>
---
+++ vueapp/src/components/ParagraphComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="para">
+    <p><b>{{ texty }}</b></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ParagraphComp',
+  props: {
+    texty: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  color: #F582A8;
+}
+</style>
